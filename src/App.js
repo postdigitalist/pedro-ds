101ececb68dde2372ff0210c4b860ab3ba044af5
Helmut Sander <helmut@Helmuts-MacBook-Pro.local>
2022-07-26 11:45:55 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { PlusCircle, Trash, MoreHorizontal } from "react-feather";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-black-10 h-screen p-14">
+      <div className="grid grid-cols-3 gap-4 justify-center items-center">
+     <button className="primaryButton">
+      Comienza ahora
+     </button>
+     <button className="primaryButton buttonSM">
+      Postula un nuevo proyecto
+     </button>
+     <button className="primaryButton buttonSM">
+      <PlusCircle className="mr-1 w-4"/>
+      Postula un nuevo proyecto
+     </button>
+     <button className="secondaryButton">
+      Revisa la documentación
+     </button>
+     <button className="secondaryButton buttonSM">
+   Invita a tus amigues
+     </button>
+     <button className="secondaryButton buttonSM">
+      <Trash className="mr-1 w-4"/>Elimina el borrador
+     </button>
+     <a href="#">Comienza ahora</a>
+     <a href="#" className="no-underline">Aprende más <span className="text-sm">↗</span></a>
+     <a href="#" className="no-underline">Volver</a>
+     <button className="secondaryButton squareButton">
+      <MoreHorizontal/>
+     </button>
+     <button className="primaryButton squareButton">
+      <MoreHorizontal/>
+     </button>
+    </div>
     </div>
   );
 }
