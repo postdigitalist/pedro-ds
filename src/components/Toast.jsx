e38e25ae148a3380cf8f803323e0c7e8f3b8ce77
Pablo Toledo Margalef <pabloatm980@gmail.com>
2022-09-08 17:31:23 -0300

    Arreglar márgenes

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -2,7 +2,7 @@ const Toast = ({ title, icon, children }) => {
   const Icon = icon;
 
   return (
-    <div className="bg-black-00 rounded-lg border border-black-00 shadow-md p-6 my-4 lg:my-0">
+    <div className="bg-black-00 rounded-lg border border-black-00 shadow-md p-6 my-4 lg:my-8 ">
       <div className="flex  items-center">
         <Icon className="text-blue-50 mr-2" />
         <h4>{title}</h4>
