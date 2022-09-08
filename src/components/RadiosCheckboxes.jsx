723c6e7e50a583f038b6215444151d6a73a90f07
Pablo Toledo Margalef <pabloatm980@gmail.com>
2022-09-08 17:34:08 -0300

    Add props to inputs

--- src/components/RadiosCheckboxes.jsx
+++ src/components/RadiosCheckboxes.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 
-const RadioField = ({ label, id }) => {
+const RadioField = ({ label, id, ...rest }) => {
   return (
     <div className="pb-1">
-      <input type="radio" id="{id}" />
-      <label htmlFor="{id}" className="ml-2 text-black-80">
+      <input type="radio" id={id} {...rest} />
+      <label htmlFor={id} className="ml-2 text-black-80">
         {label}
       </label>
     </div>
   );
 };
 
-const CheckboxField = ({ label, id }) => {
+const CheckboxField = ({ label, id, ...rest }) => {
   return (
     <div className="pb-1">
-      <input type="checkbox" />
-      <label htmlFor="{id}" className="ml-2 text-black-80">
+      <input type="checkbox" id={id} {...rest} />
+      <label htmlFor={id} className="ml-2 text-black-80">
         {label}
       </label>
     </div>
