841f700a1576a8313e91402430e22f6f4c763d0d
Pablo Toledo Margalef <pabloatm980@gmail.com>
2022-09-08 17:43:52 -0300

    Progress bar parameter
    Now it accepts a numeric value from 1-100

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-const ProgressBar = (props) =>{
-    return(
-        <div className="w-full h-2 bg-blue-20 rounded-full">
-          <div className={`bg-blue-50 h-full rounded-full ${ props.progress }`}>      
-      </div>
-      </div>
-    )
-}
-export default ProgressBar
+const ProgressBar = ({ progress }) => {
+  return (
+    <div className="w-full h-2 bg-blue-20 rounded-full">
+      <div
+        className={`bg-blue-50 h-full rounded-full`}
+        style={{ width: `${progress}%` }}
+      ></div>
+    </div>
+  );
+};
+export default ProgressBar;
