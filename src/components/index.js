9ed8be695d1a7136fbc5518c5ce7b800e49709fc
Pablo Toledo Margalef <pabloatm980@gmail.com>
2022-09-23 06:04:30 -0300

    Npm publish cleanup

---
+++ src/components/index.js
@@ -0,0 +1,68 @@
+import "../index.css";
+import Button from "./Button";
+import Breadcrumbs from "./Breadcrumbs";
+import Dropdown from "./Dropdown";
+import Link from "./Link";
+import { SavingProgress, SavedProgress, GenericWheel } from "./Loaders";
+import { DecisionModal, Modal, ModalHeader, ModalBody } from "./Modals";
+import {
+  NavHorizontal,
+  NavHorizontalMenu,
+  NavHorizontalMenuItem,
+} from "./NavHorizontal";
+import {
+  NavVerticalDesktop,
+  NavVerticalDesktopHeader,
+  NavVerticalDesktopMenu,
+  NavVerticalDesktopMenuItem,
+  NavVerticalDesktopBody,
+  NavVerticalDesktopFooter,
+  NavVerticalMobile,
+  NavVerticalMobileMenu,
+  NavVerticalMobileMenuFooter,
+  NavVerticalMobileBody,
+} from "./NavVertical";
+import ProgressBar from "./Progress";
+import { RadioField, CheckboxField } from "./RadiosCheckboxes";
+import Tag from "./Tag";
+import Task from "./Task";
+import { TextInput, TextInputError } from "./TextInput";
+import Toast from "./Toast";
+import { RoundedIcon, SquareIcon } from "./UserIcons";
+
+export {
+  Button,
+  Breadcrumbs,
+  Dropdown,
+  Link,
+  ProgressBar,
+  Tag,
+  Task,
+  Toast,
+  RadioField,
+  CheckboxField,
+  TextInput,
+  TextInputError,
+  SavingProgress,
+  SavedProgress,
+  GenericWheel,
+  DecisionModal,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  NavHorizontal,
+  NavHorizontalMenu,
+  NavHorizontalMenuItem,
+  NavVerticalDesktop,
+  NavVerticalDesktopHeader,
+  NavVerticalDesktopMenu,
+  NavVerticalDesktopMenuItem,
+  NavVerticalDesktopBody,
+  NavVerticalDesktopFooter,
+  NavVerticalMobile,
+  NavVerticalMobileMenu,
+  NavVerticalMobileMenuFooter,
+  NavVerticalMobileBody,
+  RoundedIcon,
+  SquareIcon,
+};
