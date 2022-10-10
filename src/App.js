90d803f2cfb5ceebd1a3ce6583dc0d8df1e33f0a
Helmut Sander <helmut@Helmuts-MacBook-Pro.local>
2022-10-10 19:23:46 -0300

    notice en app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 function App() {
+
   return (
     <div>
-     
+     <h1>Nada por aquí...</h1>
+    <p>Corre el comando <code>npm run storybook</code> para acceder a la librería de componentes.</p>
     </div>
   );
 }
